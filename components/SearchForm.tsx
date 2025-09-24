477511573b0af45cfad456297ec856c68d3b9469
Reckless312 <ionutcora66@gmail.com>
2025-09-24 18:49:56 +0300

    EIRA

--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -5,12 +5,7 @@ import Image from "next/image";
 const SearchForm = ({query} : {query ? : string}) => {
     return (
         <Form action="/search" scroll={false} className="search-form">
-            <input
-                name="query"
-                defaultValue={query}
-                placeholder="Search Games"
-                className="search-input"
-            />
+            <input name="query" defaultValue={query} placeholder="Search Games" className="search-input outline-none focus:ring-0 focus:outline-none"/>
             <div className="search-button">
                 <button type="submit" className="cursor-pointer">
                     <Image src="/search.png" alt="search" width={20} height={20}></Image>
